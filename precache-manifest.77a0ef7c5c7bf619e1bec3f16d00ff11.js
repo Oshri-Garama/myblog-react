202232b4e7f6f935a0dcb5b995f5a10ecae39a7a
oshripickapier <oshri@pickapier.com>
2020-05-15 01:04:56 +0300

    Updates

---
+++ precache-manifest.77a0ef7c5c7bf619e1bec3f16d00ff11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519db911ceec47a460a247f91ee4845b",
+    "url": "/myblog-react/index.html"
+  },
+  {
+    "revision": "856b88d60f52975b52e2",
+    "url": "/myblog-react/static/css/main.aba9df83.chunk.css"
+  },
+  {
+    "revision": "f3719cba2f9dae486ca0",
+    "url": "/myblog-react/static/js/2.4da4ab95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myblog-react/static/js/2.4da4ab95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856b88d60f52975b52e2",
+    "url": "/myblog-react/static/js/main.890fce59.chunk.js"
+  },
+  {
+    "revision": "176d3d4e43e8e03ce247",
+    "url": "/myblog-react/static/js/runtime-main.053cd027.js"
+  }
+]);
