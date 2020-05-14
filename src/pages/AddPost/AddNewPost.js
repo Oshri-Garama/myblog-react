69fa99d2076873069c45121ff5d7bbccad7298f6
oshripickapier <oshri@pickapier.com>
2020-05-14 23:58:12 +0300

    added history push after posting new post, and handling empty title and content

--- src/pages/AddPost/AddNewPost.js
+++ src/pages/AddPost/AddNewPost.js
@@ -1,48 +1,73 @@
-import React from 'react'
-import './AddNewPost.css'
+import React from "react";
+import "./AddNewPost.css";
+import { Redirect } from 'react-router-dom';
 
 class AddNewPost extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       post: {
-        content: '',
-        title: ''
-      }
-    }
+        content: "",
+        title: "",
+      },
+      saved: false
+    };
   }
   handleTitleChange = (event) => {
     this.setState({
       post: {
         ...this.state.post,
-        title: event.target.value
-      }
-    })
-  }
+        title: event.target.value,
+      },
+    });
+  };
 
   handleContentChange = (event) => {
     this.setState({
       post: {
         ...this.state.post,
-        content: event.target.value
-      }
-    })
-  }
+        content: event.target.value,
+      },
+    });
+  };
 
-  handleSavePost = (event) => {
+  onSubmit = (event) => {
+    event.preventDefault()
     const { post } = this.state;
-    this.props.handleAddPost(post);
+    if (post.title && post.content) {
+      this.props.handleAddPost(post)
+      alert('Post saved successfully')
+      this.props.history.push('/')
+    }
+    else {
+      alert('Title and Content are required')
+    }
   }
 
-  render () {
+  render() {
     return (
-      <div className='new-post-container'>
+      <form className="new-post-container" onSubmit={this.onSubmit}>
         <h1>Create New Post</h1>
-        <input id='input-title' type='text' placeholder='Post title goes here...' onChange={this.handleTitleChange}></input>
-        <textarea id='input-content' placeholder='Post content goes here...' onChange={this.handleContentChange}></textarea>
-        <button id='save-post-button' type='submit' onClick={this.handleSavePost}>Save post</button>
-      </div>
-    )
+        <input
+          id="input-title"
+          type="text"
+          placeholder="Post title goes here..."
+          onChange={this.handleTitleChange}
+        ></input>
+        <textarea
+          id="input-content"
+          placeholder="Post content goes here..."
+          onChange={this.handleContentChange}
+        ></textarea>
+        <button
+          id="save-post-button"
+          type="submit"
+          onClick={this.handleSavePost}
+        >
+          Save post
+        </button>
+      </form>
+    );
   }
 }
 
