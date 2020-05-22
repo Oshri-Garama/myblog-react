d3fd1a7e7389517b5016e9390502ae4f7edc3427
oshripickapier <oshri@pickapier.com>
2020-05-22 13:25:14 +0300

    added image to post page

--- front/src/pages/PostPage/PostPage.js
+++ front/src/pages/PostPage/PostPage.js
@@ -14,8 +14,10 @@ class PostPage extends React.Component {
     const currentPost = posts.find((post) => post.id == id);
     return (
       <div>
-        <h1>{currentPost.title}</h1>
-        <div>{currentPost.content}</div>
+        {console.log(currentPost)}
+        <h1 style={{marginBottom: '0'}}>{currentPost.title}</h1>
+        <img style={{position: 'relative', top: 20, right: 0, maxWidth: 300, height: 'auto'}} src={currentPost.image} alt="" />
+        <div style={{marginTop: '50px'}}>{currentPost.content}</div>
       </div>
     );
   }
