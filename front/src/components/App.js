d2d741acb6458b11338f1fe988178fc877960afb
oshripickapier <oshri@pickapier.com>
2020-06-19 00:22:06 +0300

    added localstorage to add new post as well so the page fetch data fast

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -63,6 +63,7 @@ class App extends React.Component {
       posts: posts
     })
     this.getAllPosts()
+    localStorage.setItem('session', JSON.stringify(this.state))
   };
 
   getAllPosts = () => {
