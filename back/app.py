eec25dd8f1bf7ce01d898600b9506a8530fd38ee
oshripickapier <oshri@pickapier.com>
2020-06-22 01:50:05 +0300

    changed the db to the RDS one

--- back/app.py
+++ back/app.py
@@ -3,17 +3,10 @@ import mysql.connector as mysql
 import uuid
 import bcrypt
 
-# db = mysql.connect(
-#     host = "blog-db.caobksrxxsqg.us-east-1.rds.amazonaws.com",
-#     user = "admin",
-#     passwd = "Oshri123456",
-#     database = "blog"
-# )
-
 db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "123456",
+    host = "blog-db.caobksrxxsqg.us-east-1.rds.amazonaws.com",
+    user = "admin",
+    passwd = "Oshri123456",
     database = "blog"
 )
 
