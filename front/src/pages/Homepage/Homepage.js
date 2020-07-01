eb7d8a3f279067026a42ae79f0cf3c979bf00df7
oshripickapier <oshri@pickapier.com>
2020-07-01 10:51:38 +0300

    popular posts up

--- front/src/pages/Homepage/Homepage.js
+++ front/src/pages/Homepage/Homepage.js
@@ -13,7 +13,8 @@ class Homepage extends React.Component {
     const { posts, userId, isLoggedIn } = this.props;
     return (
       <div id="home-container">
-        <div id='seperator-center'>
+        <TopFive/>
+        {/* <div id='seperator-center'>
           <div id='seperator-top'>
             <div id='top-five'>
               <header id='top-five-title'>TOP FIVE</header>
@@ -21,15 +22,15 @@ class Homepage extends React.Component {
             </div>
           </div>
         <div id='footer-page-container'>
-          <div id='recent-posts-container'>
+          <div id='recent-posts-container'> */}
             {/* <header id="blog-title">Latests Posts</header> */}
-            <div id='posts-container'>
+            {/* <div id='posts-container'>
               <Posts posts={posts} isLoggedIn={isLoggedIn} userId={userId} />
             </div>
           </div>
             <Sidebar isLoggedIn={isLoggedIn} />
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
