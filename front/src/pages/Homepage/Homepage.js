3b3536522c4cddec088df74113fe258aa73baa5f
oshripickapier <oshri@pickapier.com>
2020-07-03 11:20:47 +0300

    render homepage in two states and styling for guest-mode homepage

--- front/src/pages/Homepage/Homepage.js
+++ front/src/pages/Homepage/Homepage.js
@@ -7,46 +7,76 @@ import { Link } from "react-router-dom";
 import newPostSVG from "../../images/newPost.svg";
 import allPostsSVG from "../../images/allPosts.svg";
 import myPostsSVG from "../../images/myPosts.svg";
-import disableScroll from 'disable-scroll';
-
-
+import signInSVG from "../../images/signin.svg";
+import signUpSVG from "../../images/signup.svg";
+import writerSVG from "../../images/writer.svg";
 
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount = () => {
-  }
-
-  render() {
-    const { posts, userId, isLoggedIn } = this.props;
+  renderUserHomepage = () => {
     return (
-      <div id="home-container">
-        <div id='title-container'>
-          <header id='web-title'>Writee</header>
-          <header id='web-subtitle'>Memories Don't Last Forever</header>
+      <div id="buttons-homepage">
+        <div className="button-container">
+          <Link className="button-homepage" to="/posts/new">
+            <img src={newPostSVG} />
+          </Link>
+          <text>New Post</text>
         </div>
-        <div id="buttons-homepage">
-          <div className='button-container'>
-            <Link className="button-homepage" to="/posts/new">
-              <img src={newPostSVG} />
-            </Link>
-            <text>New Post</text>
-          </div>
-          <div className='button-container'>
-            <Link className="button-homepage" to="/posts">
-              <img src={allPostsSVG} />
-            </Link>
-            <text>All Posts</text>
-          </div>
-          <div className='button-container'>
+        <div className="button-container">
+          <Link className="button-homepage" to="/posts">
+            <img src={allPostsSVG} />
+          </Link>
+          <text>All Posts</text>
+        </div>
+        <div className="button-container">
           <Link className="button-homepage" to="/">
             <img src={myPostsSVG} />
           </Link>
           <text>My Posts</text>
-          </div>
         </div>
+      </div>
+    );
+  };
+
+  renderGuestHomepage = () => {
+    return (
+      <div id="buttons-homepage">
+        <div className="button-container">
+          <Link className="button-homepage" to="/login">
+            <img src={signInSVG} />
+          </Link>
+          <text>Sign In</text>
+        </div>
+        <div className="button-container">
+          <Link className="button-homepage" to="/signup">
+            <img src={signUpSVG} />
+          </Link>
+          <text>Sign Up</text>
+        </div>
+        <div className="button-container">
+          <Link className="button-homepage" to="/about">
+            <img src={writerSVG} />
+          </Link>
+          <text>About Me</text>
+        </div>
+      </div>
+    );
+  };
+
+  componentDidMount = () => {};
+
+  render() {
+    const { posts, userId, isLoggedIn } = this.props;
+    return (
+      <div className="home-container">
+        <div id="title-container">
+          <header id="web-title">Writee</header>
+          <header id="web-subtitle">Memories Don't Last Forever</header>
+        </div>
+        {isLoggedIn ? this.renderUserHomepage() : this.renderGuestHomepage()}
       </div>
     );
   }
