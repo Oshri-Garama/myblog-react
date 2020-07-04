cde9ea4d00056496eecd03224a66b4f412b8c44f
oshripickapier <oshri@pickapier.com>
2020-07-04 10:38:03 +0300

    about me page is styled

--- front/src/pages/AboutMe/AboutMe.js
+++ front/src/pages/AboutMe/AboutMe.js
@@ -3,15 +3,15 @@ import './aboutMe.css'
 
 const AboutMe = () => {
   return (
-    <div>
-      <h1>About me</h1>
+    <div id='aboutme-container'>
+      <header>Welcome To My Blog Application!</header>
       <p>
         Welcome to my blog application! {'\n'}
         My name is Oshri Garama, I live in Israel in Tel Aviv city. {'\n'}
         I love technology and I have a BS.c degree in Computer Science from the "IDC", Herzliya, {'\n'}
         Today I am working as a Full Stack Developer and QA at "Pick A Pier" company, which provides a technology solution for the marina industry. {'\n'}
         I have created this app as part of full stack development's course in the IDC, where I learned alot about: {'\n'} 
-        Python, React, JS, CSS...
+        React, Javascript, MySQL, Python, CSS
       </p>
     </div>
   )
