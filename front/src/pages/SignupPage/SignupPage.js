b6250ffe534c28c6cdde877a8f850018a3d4694b
oshripickapier <oshri@pickapier.com>
2020-07-04 10:25:17 +0300

    designed sign up page and some fixes in login page

--- front/src/pages/SignupPage/SignupPage.js
+++ front/src/pages/SignupPage/SignupPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./SignupPage.css";
-import axios from 'axios'
+import axios from "axios";
+import signupSVG from "../../images/signup.svg";
 
 // const port = '5000';
 // const baseUrl = `http://ec2-54-209-175-208.compute-1.amazonaws.com:${port}`;
 
-class LoginPage extends React.Component {
+class SignupPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,80 +19,96 @@ class LoginPage extends React.Component {
   handleFullNameChange = (event) => {
     this.setState({
       ...this.state,
-      fullName: event.target.value
-    })
+      fullName: event.target.value,
+    });
   };
 
   handleUserNameChange = (event) => {
     this.setState({
       ...this.state,
-      username: event.target.value
-    })
+      username: event.target.value,
+    });
   };
 
   handlePasswordChange = (event) => {
     this.setState({
       ...this.state,
-      password: event.target.value
-    })
+      password: event.target.value,
+    });
   };
 
   handleSignup = (event) => {
     event.preventDefault();
-    const {fullName, username, password} = this.state
-    if (username === '' || password === '' || fullName === '') {
-      alert('Please provide a valid input')
-      return
+    const { fullName, username, password } = this.state;
+    if (username === "" || password === "" || fullName === "") {
+      alert("Please provide a valid input");
+      return;
     }
-    axios.post('/api/signup' ,{fullName, username, password }).then(res => {
-      if (res.status === 200) {
-        this.props.handleSignup(res.data)
-        this.props.history.push('/')
-      }
-    }).catch(err => {
-      if (err.response.status === 404) {
-        alert('There is a problem connecting to the server, please contact the administrator')
-        console.log(err, 'Couldn\'t connect to database')
-      }
-      else if (err.response.status === 409) {
-        alert('The username is already exist, please choose another one')
-        this.refs.form.reset()
-      }
-    })
-  }
+    axios
+      .post("/api/signup", { fullName, username, password })
+      .then((res) => {
+        if (res.status === 200) {
+          this.props.handleSignup(res.data);
+          this.props.history.push("/");
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          alert(
+            "There is a problem connecting to the server, please contact the administrator"
+          );
+          console.log(err, "Couldn't connect to database");
+        } else if (err.response.status === 409) {
+          alert("The username is already exist, please choose another one");
+          this.refs.form.reset();
+        }
+      });
+  };
 
   render() {
     return (
-      <form className="login-page-container" onSubmit={this.handleSignup} ref='form'>
-        <h1>Sign Up</h1>
-        <div className='form-container'>
-          <div className='fields-container'>
-            <div>Full Name:</div>
-            <div>User name:</div>
-            <div>Password:</div>
+      <form id="signup-page-container" onSubmit={this.handleSignup} ref="form">
+        <header id="signup-title">Start Your Journey Now!</header>
+        <section id="signup-description">
+          In Writee, you can write every single memory that you experienced
+          through your life{"\n"}
+          As a member you will have the ability to post private or public posts
+          and read other's memories{"\n"}
+          So what are you waiting for?{"\n"}
+        </section>
+        <div id="signup-form-container">
+          <div id="signup-image-sepeartor">
+            <img src={signupSVG}/>
+            <div id='signup-fields-container'>
+            <div id="signup-labels-container">
+              <div>Full Name:</div>
+              <div>User name:</div>
+              <div>Password:</div>
+            </div>
+            <div id="signup-inputs-container">
+              <input
+                className="field-decoration"
+                type="text"
+                placeholder="Type here your full name..."
+                onChange={this.handleFullNameChange}
+              ></input>
+              <input
+                className="field-decoration"
+                type="text"
+                placeholder="Type here your user name..."
+                onChange={this.handleUserNameChange}
+              ></input>
+              <input
+                className="field-decoration"
+                type="password"
+                placeholder="Type here your password..."
+                onChange={this.handlePasswordChange}
+              ></input>
+            </div>
           </div>
-          <div className='inputs-container'>
-            <input
-              className="field-decoration"
-              type="text"
-              placeholder="Type here your full name..."
-              onChange={this.handleFullNameChange}
-            ></input>
-            <input
-              className="field-decoration"
-              type="text"
-              placeholder="Type here your user name..."
-              onChange={this.handleUserNameChange}
-            ></input>
-            <input
-              className="field-decoration"
-              type="password"
-              placeholder="Type here your password..."
-              onChange={this.handlePasswordChange}
-            ></input>
-          </div>
+            </div>
         </div>
-        <button className="login-button" type="submit">
+        <button id="signup-button" type="submit">
           Sign Up
         </button>
       </form>
@@ -99,4 +116,4 @@ class LoginPage extends React.Component {
   }
 }
 
-export default LoginPage;
+export default SignupPage;
