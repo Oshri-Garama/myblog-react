6ab680dd94da9a5403bded533639f54abf4a65e1
oshripickapier <oshri@pickapier.com>
2020-09-03 09:30:28 +0300

    trim empty lines in the begining of content in posts page

--- front/src/utils/utils.js
+++ front/src/utils/utils.js
@@ -2,6 +2,6 @@ export const fromHTMLtoString = (htmlData) => {
   let parsedData = `${htmlData.replace(/<\/p>/g, '\n')}`
   parsedData = `${parsedData.replace(/<.*>|<\/.*>/g, '')}`
   parsedData = `${parsedData.replace(/&nbsp;/g, ' ')}`
-  console.log('*begin', parsedData, 'end***')
+  parsedData = parsedData.trim()
   return parsedData
 }
