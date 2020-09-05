dbbbd43126ca250087abfae342f3a48a0f218d50
oshripickapier <oshri@pickapier.com>
2020-09-05 14:11:41 +0300

    Edit post page is now working properly

--- front/src/pages/EditPost/EditPost.js
+++ front/src/pages/EditPost/EditPost.js
@@ -4,9 +4,12 @@ import axios from "axios";
 import editSVG from "../../images/edit-logo.svg";
 import AlertMessage from "../../components/AlertMessage/AlertMessage";
 import TagsSelector from "../../components/TagsSelector/TagsSelector";
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import '../../styles/editor.css';
+import CKEditor from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "../../styles/editor.css";
+import { storage } from "../../firebase";
+import { v4 as uuid } from "uuid";
+import xbuttonSVG from "../../images/icons/xbutton.svg";
 
 class EditPost extends React.Component {
   constructor(props) {
@@ -17,14 +20,16 @@ class EditPost extends React.Component {
         content: "",
         title: "",
         imageUrl: "",
-        tags: []
+        image: "",
+        tags: [],
       },
       popup: {
         message: null,
         isPopupOpen: false,
         success: false,
       },
-      isLoading: true
+      isLoading: true,
+      progressUploadingImage: 0,
     };
   }
   handleTitleChange = (event) => {
@@ -49,15 +54,56 @@ class EditPost extends React.Component {
     this.setState({
       post: {
         ...this.state.post,
-        imageUrl: event.target.value,
+        image: event.target.files[0],
       },
     });
   };
 
-  onSubmit = (event) => {
+  onSubmit = async (event) => {
     event.preventDefault();
     const { post } = this.state;
-    if (post.title && post.content && post.title.length <= 30) {
+    const { image, title, content } = post;
+    if (image && title && content && title.length <= 30) {
+      const randomString = uuid();
+      const uploadTask = storage
+        .ref(`/images/${randomString}-${image.name}`)
+        .put(image);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          this.setState({
+            ...this.state,
+            progressUploadingImage: progress,
+          });
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          storage
+            .ref("images")
+            .child(`${randomString}-${image.name}`)
+            .getDownloadURL()
+            .then((url) => {
+              this.editPost(url);
+            });
+        }
+      );
+    } else {
+      this.editPost();
+    }
+  };
+
+  editPost = (imageUrl) => {
+    const { post } = this.state;
+    const { title, content } = post;
+    if (imageUrl) {
+      post.imageUrl = imageUrl;
+    }
+    if (title && content && title.length <= 30) {
       axios.put("/api/posts", post).then((res) => {
         if (res.status === 200) {
           post.published = res.data.post.published;
@@ -72,21 +118,19 @@ class EditPost extends React.Component {
           });
           setTimeout(() => {
             this.props.history.push(`/posts/${post.id}`);
-          }, 3000)
+          }, 3000);
         }
       });
-    } 
-    else if (post.title.length > 30) {
+    } else if (post.title.length > 30) {
       this.setState({
         ...this.state,
         popup: {
           message: "Title must be 30 letters max",
           isPopupOpen: true,
           success: false,
-        }
-      })
-    }
-    else {
+        },
+      });
+    } else {
       this.setState({
         ...this.state,
         popup: {
@@ -118,17 +162,17 @@ class EditPost extends React.Component {
       ...this.state,
       post: {
         ...this.state.post,
-        tags
-      }
-    })
-  }
+        tags,
+      },
+    });
+  };
 
   componentDidMount = () => {
-    const { id } = this.props.location.state
-    axios.get(`/api/posts/${id}`).then(res => {
+    const { id } = this.props.location.state;
+    axios.get(`/api/posts/${id}`).then((res) => {
       if (res.status === 200) {
-        const { content, title, imageUrl } = res.data.post
-        const { tags } = res.data
+        const { content, title, imageUrl } = res.data.post;
+        const { tags } = res.data;
         this.setState({
           ...this.state,
           post: {
@@ -139,28 +183,47 @@ class EditPost extends React.Component {
             imageUrl,
           },
           isLoading: false,
-        })
+        });
+      }
+    });
+  };
+
+  renderTagSelector = () => {
+    const { isLoading } = this.state;
+    const { tags } = this.state.post;
+    if (tags.length) {
+      return (
+        <TagsSelector
+          getSelectedTags={this.getSelectedTags}
+          action="update"
+          tags={tags}
+        />
+      );
+    } else if (this.state.post.tags.length === 0 && !isLoading) {
+      return (
+        <TagsSelector
+          getSelectedTags={this.getSelectedTags}
+          action="update"
+          tags={[]}
+        />
+      );
+    }
+  };
+
+  removePicture = () => {
+    this.setState({
+      ...this.state,
+      post: {
+        ...this.state.post,
+        image: '',
+        imageUrl: ''
       }
     })
   }
 
-  renderTagSelector = () => {
-    const { isLoading } = this.state
-    const { tags } = this.state.post
-    if (tags.length) {
-      return (
-        <TagsSelector getSelectedTags={this.getSelectedTags} action='update' tags={tags} />
-      )
-    }
-    else if (this.state.post.tags.length === 0 && !isLoading) {
-      return (
-        <TagsSelector getSelectedTags={this.getSelectedTags} action='update' tags={[]} />
-      )
-    }
-  }
-
   render() {
-    const { title, content, imageUrl } = this.state.post;
+    const { progressUploadingImage, post } = this.state;
+    const { title, content, image, imageUrl } = post;
     const { message, success } = this.state.popup;
     const type = success ? "success" : "failed";
     this.closePopupIfOpen();
@@ -177,25 +240,46 @@ class EditPost extends React.Component {
             placeholder="Post title goes here..."
             onChange={this.handleTitleChange}
           ></input>
-          <input
-            id="input-add-img"
-            value={imageUrl}
-            type="url"
-            placeholder="Paste here your image url"
-            onChange={this.handleImageChange}
-          ></input>
-          <div id='ck-editor'>
+          <div id="ck-editor">
             <CKEditor
               data={content}
               editor={ClassicEditor}
-              config={{removePlugins: [ 'List', 'Table', 'MediaEmbed', 'BlockQuote', 'Indent']}}
+              config={{
+                removePlugins: [
+                  "List",
+                  "Table",
+                  "MediaEmbed",
+                  "BlockQuote",
+                  "Indent",
+                  "ImageUpload",
+                ],
+              }}
               onChange={(event, editor) => {
                 const data = editor.getData();
-                {this.handleContentChange(data)}
+                {
+                  this.handleContentChange(data);
+                }
               }}
             />
           </div>
           {this.renderTagSelector()}
+          <div className="image-addition-input">
+            <label className="upload-pic-label" for="upload">
+              {image ? image.name : !imageUrl ? "Upload Picture" : "Edit Picture"}
+            </label>
+            <input
+              type="file"
+              id="upload"
+              accept="image/*"
+              onChange={this.handleImageChange}
+              readonly
+            ></input>
+            {imageUrl && <img src={xbuttonSVG} title={'Click to remove picture'} onClick={this.removePicture}></img>}
+          </div>
+          <div
+            className="progress-bar"
+            style={{ "--progress": `${progressUploadingImage}%` }}
+          ></div>
           <button
             id="create-post-button"
             type="submit"
