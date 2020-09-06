234ca9e79f8fddc1182d6f12030b6e03cc08cc7e
oshripickapier <oshri@pickapier.com>
2020-09-06 09:37:04 +0300

    changed LoginPage from class component to functional with hooks

--- front/src/pages/LoginPage/LoginPage.js
+++ front/src/pages/LoginPage/LoginPage.js
@@ -1,145 +1,122 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./LoginPage.css";
 import axios from "axios";
 import loginSVG from "../../images/signin.svg";
 import loginLogoSVG from "../../images/login-logo.svg";
 import AlertMessage from "../../components/AlertMessage/AlertMessage";
+import { useTranslation } from 'react-i18next'
 
-class LoginPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      popup: {
-        message: null,
-        isPopupOpen: false,
-        success: false,
-      },
-    };
-  }
+const LoginPage = (props) => {
+  const [username, setUserame] = useState("");
+  const [password, setPassword] = useState("");
+  const [popup, setPopup] = useState({
+    message: null,
+    isPopupOpen: false,
+    success: false,
+  });
+  const formRef = useRef(null);
+  const { t } = useTranslation();
 
-  handleUserNameChange = (event) => {
-    this.setState({
-      username: event.target.value,
-    });
+  const handleUserNameChange = (event) => {
+    setUserame(event.target.value);
   };
 
-  handlePasswordChange = (event) => {
-    this.setState({
-      password: event.target.value,
-    });
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
-  handleLogin = (event) => {
+  const handleLogin = (event) => {
     event.preventDefault();
-    const { username, password } = this.state;
     // added state from to save the last page and then on success redirect there.
     let lastUrlPath = "/";
-    const location = this.props.location.state;
+    const location = props.location.state;
     if (location && location.from) {
       lastUrlPath = location.from;
     }
     if (username === "" || password === "") {
-      this.setState({
-        ...this.state,
-        popup: {
-          message: "Please provide a valid input",
-          isPopupOpen: true,
-          success: false,
-        },
+      setPopup({
+        message: "Please provide a valid input",
+        isPopupOpen: true,
+        success: false,
       });
-      this.refs.form.reset();
+      formRef.current.reset();
       return;
     }
     axios
       .post("/api/login", { username, password })
       .then((res) => {
         if (res.status === 200) {
-          this.props.handleLogin(res.data);
-          this.props.history.push(lastUrlPath);
+          props.handleLogin(res.data);
+          props.history.push(lastUrlPath);
         }
       })
       .catch((err) => {
         if (err.response.status === 404) {
-          this.setState({
-            ...this.state,
-            popup: {
-              message:
-                "Problem connecting to the server, please contact support",
-              isPopupOpen: true,
-              success: false,
-            },
+          setPopup({
+            message: "Problem connecting to the server, please contact support",
+            isPopupOpen: true,
+            success: false,
           });
           console.log(err, "Couldn't connect to database");
         } else if (err.response.status === 401) {
-          this.setState({
-            ...this.state,
-            popup: {
-              message: "The username or the password you provided is incorrect",
-              isPopupOpen: true,
-              success: false,
-            },
+          setPopup({
+            message: "The username or the password you provided is incorrect",
+            isPopupOpen: true,
+            success: false,
           });
-          this.refs.form.reset();
+          formRef.current.reset();
         }
       });
   };
 
-  closePopupIfOpen = () => {
-    const { isPopupOpen } = this.state.popup;
-    if (isPopupOpen) {
+  const closePopupIfOpen = () => {
+    if (popup.isPopupOpen) {
       setTimeout(() => {
-        this.setState({
-          ...this.state,
-          popup: {
-            isPopupOpen: false,
-            message: null,
-          },
+        setPopup({
+          isPopupOpen: false,
+          message: null,
         });
       }, 4000);
     }
   };
 
-  render() {
-    const { message, success } = this.state.popup;
-    const type = success ? "success" : "failed";
-    this.closePopupIfOpen();
-    return (
-      <form id="login-container-page" onSubmit={this.handleLogin} ref="form">
-        <AlertMessage message={message} type={type} />
-        <img id="login-logo-svg" src={loginLogoSVG} />
-        <div className="form-container">
-          <div className="image-sepeartor">
-            <img src={loginSVG} />
-            <div className="fields-container">
-              <div className="labels-container">
-                <div>User name:</div>
-                <div>Password:</div>
-              </div>
-              <div id="inputs-container">
-                <input
-                  className="field-decoration"
-                  type="text"
-                  placeholder="Type here your user name..."
-                  onChange={this.handleUserNameChange}
-                ></input>
-                <input
-                  className="field-decoration"
-                  type="password"
-                  placeholder="Type here your password..."
-                  onChange={this.handlePasswordChange}
-                ></input>
-              </div>
+  const { message, success } = popup;
+  const type = success ? "success" : "failed";
+  closePopupIfOpen();
+  return (
+    <form id="login-container-page" onSubmit={handleLogin} ref={formRef}>
+      <AlertMessage message={message} type={type} />
+      <img id="login-logo-svg" src={loginLogoSVG} />
+      <div className="form-container">
+        <div className="image-sepeartor">
+          <img src={loginSVG} />
+          <div className="fields-container">
+            <div className="labels-container">
+              <div>User name:</div>
+              <div>Password:</div>
             </div>
-            <button className="login-button" type="submit">
-              Login
-            </button>
+            <div id="inputs-container">
+              <input
+                className="field-decoration"
+                type="text"
+                placeholder="Type here your user name..."
+                onChange={handleUserNameChange}
+              ></input>
+              <input
+                className="field-decoration"
+                type="password"
+                placeholder="Type here your password..."
+                onChange={handlePasswordChange}
+              ></input>
+            </div>
           </div>
+          <button className="login-button" type="submit">
+            Login
+          </button>
         </div>
-      </form>
-    );
-  }
-}
+      </div>
+    </form>
+  );
+};
 
 export default LoginPage;
