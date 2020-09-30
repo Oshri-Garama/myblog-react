6e1c8d79a0e9491eceb7402f828b9cec0584c670
oshripickapier <oshri@pickapier.com>
2020-09-30 16:18:33 +0300

    added github linkedin icons

--- front/src/pages/AboutMe/AboutMe.js
+++ front/src/pages/AboutMe/AboutMe.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './aboutMe.css'
 import { useTranslation } from "react-i18next";
+import linkedinSVG from '../../images/icons/linkedin.svg'
+import githubSVG from '../../images/icons/github.svg'
 
 const AboutMe = () => {
   const { t, i18n } = useTranslation();
@@ -11,6 +13,10 @@ const AboutMe = () => {
       <p className={i18n.language === 'he' && 'about-me-hebrew-text'}>
         {t('aboutMeDesc')}
       </p>
+      <div className='aboutme-ctas'>
+        <img className='linkedin-icon' src={linkedinSVG}/>
+        <img src={githubSVG}/>
+      </div>
     </div>
   )
 }
